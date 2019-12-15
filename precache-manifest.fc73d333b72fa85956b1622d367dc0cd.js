b750af690f34ad3c3b12547839daaa1ab24f2564
itabas016 <itabas016@gmail.com>
2019-12-15 16:06:08 +0000

    Deploying to gh-pages from master 17511faf51e59ca58ec2bba881349237f645314a

---
+++ precache-manifest.fc73d333b72fa85956b1622d367dc0cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d63d2cd1a0016f0f35e1afe3e09014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "199a3a2e1566b265e023",
+    "url": "/static/css/main.c37cb22b.chunk.css"
+  },
+  {
+    "revision": "c48afd61bd443f44c88a",
+    "url": "/static/js/2.388f2737.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.388f2737.chunk.js.LICENSE"
+  },
+  {
+    "revision": "199a3a2e1566b265e023",
+    "url": "/static/js/main.8e86c523.chunk.js"
+  },
+  {
+    "revision": "219094b1e9a6ccafb227",
+    "url": "/static/js/runtime-main.90c8811a.js"
+  }
+]);
